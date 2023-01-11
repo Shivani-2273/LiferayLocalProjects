b7796c76825fd965bec20124b961c0879650cce1
Shivani Hemnani <shivanih.inexture@gmail.com>
2023-01-11 14:13:29 +0530

    Guestbook Entry Module

---
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author root316
+ */
+public class GuestbookPortletKeys {
+
+	public static final String GUESTBOOK =
+		"com_liferay_docs_guestbook_portlet_GuestbookPortlet";
+
+}
